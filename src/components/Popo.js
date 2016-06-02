e3d4e3d595f9ff62b7eff067b21ebafc05e263b7
cutofmyjib <thedianalozano@gmail.com>
2016-06-01 23:10:28 -0700

    detail div, styling + schedule icon

--- src/components/Popo.js
+++ src/components/Popo.js
@@ -15,9 +15,13 @@ export default class Popo extends Component {
         <Link to={'popos/' + this.props.id}><img className="popo-img" src={this.props.url[0]} /></Link>
         <div className="popo-subcontainer">
           <h2><Link to={'popos/' + this.props.id}>{this.props.name}</Link></h2>
-          <div className="location">
+          <div className="popo-detail-div">
             <i className="material-icons">location_on</i>
             <p>{this.props.location}</p>
+          </div>
+          <div className="popo-detail-div">
+            <i className="material-icons">schedule</i>
+            <p>{this.props.hours}</p>
           </div>
           <p>{desc2}</p>
         </div>
