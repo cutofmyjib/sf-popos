a7328931d347b5e82f927e2a05771666b3af1959
cutofmyjib <thedianalozano@gmail.com>
2016-06-02 21:32:00 -0700

    PopoView component

--- src/components/PopoView.js
+++ src/components/PopoView.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PopoImg from './PopoImg';
+import firebase from 'firebase';
 
-export default function PopoView() {
-  return (
-    <div className="view-container">
-      <h1>POPO here</h1>
-    </div>
-  );
+var config2 = {
+    apiKey: "AIzaSyDj7DG7DU8QGYE7KUwYkeukOIbUrWOniFE",
+    authDomain: "sf-popos.firebaseapp.com",
+    databaseURL: "https://sf-popos.firebaseio.com",
+    storageBucket: "",
+    serviceAccount: "sf-popos-90d2b81d7ffa.json"
+  };
+firebase.initializeApp(config2, 'secondary');
+
+export default class PopoView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    //subscribe to firebase - database
+    //get the data and save it to photos in state
+    //bind this to function
+    firebase.database().ref(this.props.params.popoId).on('value', function(snapshot) {
+      this.setState({data: snapshot.val()})
+    }.bind(this));
+  }
+
+  componentWillUnmount() {
+    //unsubscribe from firebase
+    firebase.database().ref(this.props.params.popoId).off();
+  }
+
+  render() {
+    var imgList = this.state.data.url;
+
+    if (imgList) {
+      var firstImg = imgList.shift()
+      imgList = this.state.data.url.map(function(url) {
+        return <PopoImg url={url}/>
+      })
+    }
+
+    return (
+      <div>
+        <div className="header-div">
+          <h1 className="header-title">{this.state.data.name}</h1>
+        </div>
+        <div className="popo-container">
+          <aside className="side-content">
+            <div className="side-content-subdiv">
+              <div className="content-img">
+                <img className="popo-img" src={firstImg}/>
+              </div>
+            </div>
+          </aside>
+          <div className="main-content">
+            <div className="main-content-subdiv">
+              <div className="main-content-description">
+                <p>{this.state.data.description}</p>
+              </div>
+              {imgList}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
