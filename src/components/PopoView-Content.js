e91cfc0973dfcd9d9bda906509f269c1184db5df
cutofmyjib <thedianalozano@gmail.com>
2016-06-07 00:01:36 -0700

    add Side Content component

--- src/components/PopoView-Content.js
+++ src/components/PopoView-Content.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import PopoViewContentSide from './PopoView-Content-Side';
 import PopoImg from './PopoImg';
-import Loading from './Loading';
-import Map from './Map';
 
 export default class PopoViewContent extends Component {
   render() {
-    var firstImg = this.props.data.url.shift()
+    var sideImgHeader = this.props.data.url.shift()
     var imgList = this.props.data.url.map(function(url) {
       return <PopoImg url={url}/>
     })
@@ -13,16 +12,8 @@ export default class PopoViewContent extends Component {
     return (
       <div className="popo-container">
         <aside className="side-content">
-          <div className="side-content-subdiv">
-            <div className="content-img">
-              <img className="popo-img" src={firstImg}/>
-            </div>
-            <div className="map-container">
-              <Map  lat={this.props.data.lat}
-                    long={this.props.data.long}
-              />
-            </div>
-          </div>
+          <PopoViewContentSide  data={this.props.data}
+                                sideImgHeader={sideImgHeader} />
         </aside>
         <div className="main-content">
           <div className="main-content-subdiv">
