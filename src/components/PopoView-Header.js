1ff68537004e52a8ed49fa8eaeb5b52640c1ff57
cutofmyjib <thedianalozano@gmail.com>
2016-06-07 22:54:40 -0700

    change to header tag

--- src/components/PopoView-Header.js
+++ src/components/PopoView-Header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class PopoViewHeader extends Component {
   render() {
     return (
-        <div className="header-div">
+        <header>
           <h1 className="header-title">{this.props.name}</h1>
           <p>{this.props.location}</p>
-        </div>
+        </header>
     )
   }
 }
