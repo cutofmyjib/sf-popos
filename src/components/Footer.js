1f848f1731827a6053221c3fc0f70daf0c2b969d
cutofmyjib <thedianalozano@gmail.com>
2016-06-25 23:09:43 -0700

    edit credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ export default class Footer extends Component {
     return (
       <footer>
         <p>All photos are from
-          <a href="https://cutofmyjib.github.io/about/">yours truly</a>.
+          <a href="https://cutofmyjib.github.io/about/"> yours truly</a>.
           Information from
-          <a href="https://www.spur.org/sites/default/files/migrated/anchors/popos-guide.pdf"> SPUR</a>
+          <a href="https://www.spur.org/sites/default/files/migrated/anchors/popos-guide.pdf"> SPUR</a>.
         </p>
         <p>Made with &hearts; by <a href="http://cutofmyjib.github.io/about/">cutofmyjib</a></p>
       </footer>
