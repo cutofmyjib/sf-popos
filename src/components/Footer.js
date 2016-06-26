302d1a2fdeddeb2ef311e06805c9aef1de25e851
cutofmyjib <thedianalozano@gmail.com>
2016-06-25 23:04:11 -0700

    photo creds

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>Photos are not mine, all of them come from several
-          <a href="https://www.google.com/search?q=sf+popos&espv=2&biw=751&bih=667&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiyivmL4JzNAhUS12MKHajXCsEQ_AUIBygC#imgrc=DhgwvR-7Sl5qQM%3A"> SF POPOS websites. </a>
-          Information is from
+        <p>All photos are from
+          <a href="https://cutofmyjib.github.io/about/">yours truly</a>.
+          Information from
           <a href="https://www.spur.org/sites/default/files/migrated/anchors/popos-guide.pdf"> SPUR</a>
         </p>
         <p>Made with &hearts; by <a href="http://cutofmyjib.github.io/about/">cutofmyjib</a></p>
