9df9d2c2ff5acb09caf259f981e31cf404764cf5
cutofmyjib <thedianalozano@gmail.com>
2016-06-28 00:45:22 -0700

    sources and copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,13 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>All photos are from
-          <a href="https://cutofmyjib.github.io/about/"> yours truly</a>.
+        <p>All images © 2016
+          <a href="http://cutofmyjib.com/about"> Diana Lozano</a>.
           Information from
+          <a href="http://sf-planning.org/privately-owned-public-open-space-and-public-art-popos"> SF Planning</a> and
           <a href="https://www.spur.org/sites/default/files/migrated/anchors/popos-guide.pdf"> SPUR</a>.
         </p>
-        <p>Made with &hearts; by <a href="http://cutofmyjib.github.io/about/">cutofmyjib</a></p>
+        <p>Made with &hearts; by <a href="http://cutofmyjib.com">cutofmyjib</a></p>
       </footer>
     );
   }
