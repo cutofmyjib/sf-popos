bd506a736b631aeec3e532c6681d292e99831904
cutofmyjib <thedianalozano@gmail.com>
2016-07-07 12:11:45 -0700

    make animate function

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+
 export default class Loading extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadingState: ""
+    };
+  }
+
+  animate() {
+    this.ellipsis = setInterval(function() {
+      this.setState({loadingState: this.state.loadingState += "."})
+    }.bind(this), 200)
+  };
+
+  componentDidMount() {
+    this.animate()
+    console.log(this.state.loadingState)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ellipsis)
+  }
+
   render() {
     return (
       <div className="status-div">
-        Loading
+        {this.state.loadingState}
       </div>
     )
   }
